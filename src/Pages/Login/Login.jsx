3ee5d278770cd521d90510a9fc335f138552c540
deepak <sharmads2001@gmail.com>
2025-08-14 14:31:58 +0530

    update login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -14,8 +14,15 @@ export default function Login() {
 
   return (
     <div>
-      <h1>Login Page</h1>
-      <button onClick={handleLogin}>Login</button>
+      {/* <h1>Login Page</h1>
+      <button onClick={handleLogin}>Login</button> */}
+      <div className="fixed flex w-full h-screen ">
+        {/* left side */}
+        <div className="w-1/2 h-full  bg-[linear-gradient(120deg,red,orange,#ffb3b3)]"></div>
+
+        {/* right side */}
+        <div className="w-1/2 h-full bg-gray-200"></div>
+      </div>
       
     </div>
   );
